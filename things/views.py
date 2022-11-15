7ef01f677981e8f4ce46fc53d30960a0369208da
Luca Boccardo <luca.boccardo@kcl.ac.uk>
2022-11-15 23:30:09 +0000

    Complete form

--- things/views.py
+++ things/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ThingForm
+
 
 def home(request):
-    return render(request, 'home.html', {'form': form})
+    if request.method == "POST":
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect("home")
+    else:
+        form = ThingForm()
+    return render(request, "home.html", {"form": form})
